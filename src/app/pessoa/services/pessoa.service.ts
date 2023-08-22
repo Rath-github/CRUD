cdcc3f973f419c9a1411f6e33112d9c639a95c50
IamThiago-IT <thiagodossantos315@gmail.com>
2023-08-22 00:17:48 -0300

    service class preparation

--- src/app/pessoa/services/pessoa.service.ts
+++ src/app/pessoa/services/pessoa.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Pessoa } from '../../shared/models/pessoa.model';
+
+const LS_CHAVE: string = 'pessoas';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PessoaService {
-
-  constructor() { }
+  constructor() {}
 }
