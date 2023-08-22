9fcfa012920d5715b49eb98fe40791de3b3142e8
IamThiago-IT <thiagodossantos315@gmail.com>
2023-08-22 00:04:42 -0300

    Refactor: Import Services in module

--- src/app/pessoa/pessoa.module.ts
+++ src/app/pessoa/pessoa.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { PessoaService } from './services/pessoa.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [PessoaService],
 })
-export class PessoaModule { }
+export class PessoaModule {}
